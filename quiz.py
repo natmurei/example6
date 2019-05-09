65ff463fe42fe4cd037a27c3438ecead106aba85
natmurei <47848668+natmurei@users.noreply.github.com>
2019-05-09 18:59:42 +0300

    added new file

---
+++ quiz.py
@@ -0,0 +1,37 @@
+def list comprehension():
+	a=[100,110,120,130,140,150]:
+	h=a/3
+	print(h)
+	
+	
+def sorted():
+	a=['apple','banana','mango']
+	b=['avocado','peach','orange']
+    c=['pineapple','lemon','guava']
+    d=set(a+b+c)
+    print(d)
+
+def divisible_by_three(n):
+	divisible=range(1,n%3)
+	final_divisible=division(divisible)
+	print(final_divisible)
+	
+
+def list():
+	x=[[1,2],[3,4],[5,6]]
+	h=[]
+	for sublist in x
+		for h in sublist:
+			print(h.append(x))
+
+def smallest():
+	n=[1,3,4,6,8,9]
+	print (min(n))
+
+
+def duplicate():
+	s=['a','b','a','e','d','b','c','e','f','g','h']
+	print(s.sort())
+
+
+	
